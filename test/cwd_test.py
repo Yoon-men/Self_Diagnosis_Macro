464f150d6c8bf4218313746b2b7e79b14fcb7dd2
Yoonmen <sgu101310@naver.com>
2023-01-16 02:31:09 +0900

    chesto

--- test/cwd_test.py
+++
@@ -1,8 +0,0 @@
-import os
-import keyboard
-
-print("[system] Current Working Directory : ", os.getcwd(), "\n")      # cwd = Current Working Directory
-print("[system] ESC키를 입력하면 프로그램을 종료합니다.")
-while True : 
-    if keyboard.is_pressed('esc') : 
-        break
